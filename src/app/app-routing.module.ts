e224539609464afc2f4b0954023a4fc7008d8515
panminxiang <1061886912@qq.com>
2021-09-06 17:21:59 +0800

    重写登录界面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 const routes: Routes = [
   {
@@ -23,7 +24,12 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [RouterModule.forRoot(routes), FormsModule, ReactiveFormsModule],
+  imports: [
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,96 +1,43 @@
-import { HttpHeaders } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { catchError } from 'rxjs/operators';
-import { Observable } from 'rxjs/internal/Observable';
-import { of } from 'rxjs/internal/observable/of';
-import { User } from '../model/user.model';
-import { Router } from '@angular/router';
-import { HowellAuthHttpService } from '../request/howell-auth-http.service';
-import { Digest } from '../request/digest';
-import { SessionUser } from '../common/tool/session-user';
+/*
+ * @Author: pmx
+ * @Date: 2021-09-06 17:08:43
+ * @Last Modified by:   pmx
+ * @Last Modified time: 2021-09-06 17:08:43
+ */
+
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import videojs, { VideoJsPlayer } from 'video.js';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.less'],
 })
-export class LoginComponent implements OnInit {
-  sessionUser: SessionUser = new SessionUser();
+export class LoginComponent implements AfterViewInit {
+  @ViewChild('loginVideo')
+  video?: ElementRef;
+  constructor() {}
 
-  formGroup = new FormGroup({
-    uname: new FormControl('pmx'),
-    upass: new FormControl('1212'),
-  });
-  constructor(
-    private httpService: HowellAuthHttpService,
-    private _router: Router
-  ) {}
-
-  ngOnInit(): void {}
-
-  login() {
-    this.auth();
-  }
-  auth() {
-    return this.httpService
-      .auth(
-        '/howell/ver10/data_service/user_system/Users/Login/guangzhonglu',
-        new HttpHeaders({ 'X-WebBrowser-Authentication': 'Forbidden' })
-      )
-      .pipe(catchError(this.handleLoginError<any>()))
-      .toPromise();
-  }
-  handleLoginError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      if (error.status == 403) {
-        let header = error.headers as Headers,
-          userUrl =
-            '/howell/ver10/data_service/user_system/Users/Login/guangzhonglu';
-        let digest = new Digest(
-          header,
-          '/howell/ver10/data_service/user_system/'
-        );
-        var challenge = digest.parseServerChallenge();
-        let authHeader = digest.generateRequestHeader(
-          1,
-          challenge,
-          'guangzhonglu',
-          'yxotkccu7rc3ai1h',
-          'GET',
-          userUrl
-        );
-
-        this.httpService
-          .auth(userUrl, authHeader)
-          .pipe(catchError(this.handleLoginError2<any>()))
-          .subscribe((result: User) => {
-            if (result) {
-              console.log(result);
-
-              this.memory('guangzhonglu', 'yxotkccu7rc3ai1h', result.Id);
-
-              this._router.navigateByUrl('waste');
-              // sessionStorage.setItem('userid', );
-            }
-          });
+  ngAfterViewInit() {
+    const _this = this;
+    if (!this.video) return;
+    videojs(
+      this.video.nativeElement,
+      {
+        autoplay: true,
+        loop: true,
+        controls: false,
+        muted: true,
+        sources: [
+          {
+            src: 'assets/img/login.mp4',
+            type: 'video/mp4',
+          },
+        ],
+      },
+      function onPlayerReady(this: VideoJsPlayer) {
+        console.log('onPlayerReady', this);
       }
-      return of(result as T);
-    };
-  }
-
-  handleLoginError2<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      if (error.status == 403) {
-        console.log(403);
-        // MessageBar.response_Error('账号或密码错误');
-      }
-
-      return of(result as T);
-    };
-  }
-  memory(name: string, pwd: string, id: string) {
-    this.sessionUser.name = name;
-    this.sessionUser.pwd = pwd;
+    );
   }
 }
