bc8fc35604796fc12314e2c1daee27268d935c02
panminxiang <1061886912@qq.com>
2021-09-06 13:30:04 +0800

    增加了代理配置文件

---
+++ src/proxy.conf.ts
@@ -0,0 +1,9 @@
+const PROXY_CONFIG = [
+  {
+    context: ['/howell/ver10/data_service/', '/api/howell/ver10/aiop_service/'],
+    target: 'http://garbage01.51hws.com',
+    secure: false,
+  },
+];
+
+module.exports = PROXY_CONFIG;
