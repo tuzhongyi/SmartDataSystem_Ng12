423d9109954b661f9c82b5338ddf310646b7ada9
panminxiang <1061886912@qq.com>
2022-01-05 14:47:46 +0800

    pmx

--- src/app/common/components/paginator/paginator.component.ts
+++ src/app/common/components/paginator/paginator.component.ts
@@ -5,20 +5,31 @@
  * @Last Modified time: 2021-12-30 15:51:51
  */
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
+  ComponentFactory,
+  ComponentFactoryResolver,
+  ElementRef,
   Inject,
+  Injector,
+  Input,
   OnInit,
   Optional,
+  Renderer2,
+  ViewChild,
+  ViewContainerRef,
   ViewEncapsulation,
 } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { ThemePalette } from '@angular/material/core';
 import {
   MatPaginatorDefaultOptions,
   MAT_PAGINATOR_DEFAULT_OPTIONS,
   _MatPaginatorBase,
 } from '@angular/material/paginator';
+import { TimeComponent } from '../time/time.component';
 import { PaginatorIntl } from './paginator-intl';
 
 @Component({
@@ -31,10 +42,24 @@ import { PaginatorIntl } from './paginator-intl';
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class PaginatorComponent extends _MatPaginatorBase<MatPaginatorDefaultOptions> {
-  color: ThemePalette = 'primary';
-  count: number = 0;
+export class PaginatorComponent
+  extends _MatPaginatorBase<MatPaginatorDefaultOptions>
+  implements AfterViewInit
+{
+  @Input() currentCount = 0;
+  @ViewChild('actionContainer')
+  actionContainer?: ElementRef;
+
+  @ViewChild('nextPageNode')
+  nextPageNode?: MatButton;
+
+  @ViewChild('buttonContainer', { read: ViewContainerRef })
+  buttonContainer?: ViewContainerRef;
+
   constructor(
+    private _injector: Injector,
+    private _renderer: Renderer2,
+    private resolver: ComponentFactoryResolver,
     public intl: PaginatorIntl,
     changeDetectorRef: ChangeDetectorRef,
     @Optional()
@@ -43,9 +68,23 @@ export class PaginatorComponent extends _MatPaginatorBase<MatPaginatorDefaultOpt
   ) {
     super(intl, changeDetectorRef, defaults);
   }
-  // override ngOnInit(): void {
-  //   super.ngOnInit();
-  //   console.log('on init');
-  //   console.log(this._intl);
-  // }
+  override ngOnInit(): void {
+    super.ngOnInit();
+  }
+  ngAfterViewInit(): void {
+    console.log(this.buttonContainer);
+    let div = this._renderer.createElement('div');
+    div.innerHTML = '4';
+    const factory: ComponentFactory<MatButton> =
+      this.resolver.resolveComponentFactory(MatButton);
+    let componentRef = this.buttonContainer?.createComponent(
+      factory,
+      0,
+      this._injector,
+      [[div]]
+    );
+    console.log(componentRef);
+  }
+
+  private _createButton(index: number) {}
 }
