96fa2f93e3b5ee47a052a5c64b2d997fe4fe693c
panminxiang <1061886912@qq.com>
2022-06-29 14:21:07 +0800

    pmx

--- src/app/aiop-system/components/ai-model-operate/ai-model-operate.component.ts
+++ src/app/aiop-system/components/ai-model-operate/ai-model-operate.component.ts
@@ -1,36 +1,46 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, Inject, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Inject,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { FormState } from 'src/app/enum/form-state.enum';
-import { CameraAIModel, CameraAIModelDTO, CameraAIModelDTOLabel, EnumValue } from 'src/app/network/model/camera-ai.model';
+import {
+  CameraAIModel,
+  CameraAIModelDTO,
+  CameraAIModelDTOLabel,
+  EnumValue,
+} from 'src/app/network/model/camera-ai.model';
 import { AIModelOperateBusiness } from './ai-model-operate.business';
-import Icons from "src/assets/json/ai-icon.json"
+import Icons from 'src/assets/json/ai-icon.json';
 import { BehaviorSubject, fromEvent, Subject, Subscription } from 'rxjs';
-import { DOCUMENT } from '@angular/common'
+import { DOCUMENT } from '@angular/common';
 import { ToastrService } from 'ngx-toastr';
-import { Camera } from 'src/app/network/model/camera.model';
-import { TreeConverter } from 'src/app/converter/tree.converter';
-import { MatTreeNestedDataSource } from '@angular/material/tree';
-import { NestedTreeControl } from '@angular/cdk/tree';
 
 export interface NestedTreeNode {
   name: string;
-  children?: Array<NestedTreeNode>
+  children?: Array<NestedTreeNode>;
 }
 
 @Component({
   selector: 'howell-ai-model-operate',
   templateUrl: './ai-model-operate.component.html',
   styleUrls: ['./ai-model-operate.component.less'],
-  providers: [
-    AIModelOperateBusiness
-  ]
+  providers: [AIModelOperateBusiness],
 })
-export class AIModelOperateComponent implements OnInit, AfterViewInit, OnDestroy {
-
+export class AIModelOperateComponent
+  implements OnInit, AfterViewInit, OnDestroy
+{
   private _cameraAIModel?: CameraAIModel;
-  private _parsedCameraAIModel?: CameraAIModel;
+  private _parsedAIModel?: CameraAIModel;
   private _aiModelDTo?: CameraAIModelDTO;
-
 
   FormState = FormState;
   myForm = new FormGroup({
@@ -40,8 +50,8 @@ export class AIModelOperateComponent implements OnInit, AfterViewInit, OnDestroy
     Version: new FormControl({ value: '', disabled: true }),
     TransformType: new FormControl({ value: '', disabled: true }),
     ModelJson: new FormControl(''),
-    Label: new FormControl('', [Validators.required])
-  })
+    Label: new FormControl('', [Validators.required]),
+  });
 
   showList = false;
   imgBase = 'assets/img/ai-model/';
@@ -49,12 +59,12 @@ export class AIModelOperateComponent implements OnInit, AfterViewInit, OnDestroy
 
   iconsMap = new Map(Object.entries(Icons));
   iconsValue = Array.from(this.iconsMap.values());
-  iconsKey = Array.from(this.iconsMap.keys())
-  iconsEntries = Array.from(this.iconsMap.entries())
+  iconsKey = Array.from(this.iconsMap.keys());
+  iconsEntries = Array.from(this.iconsMap.entries());
 
   maskSub!: Subscription;
 
-  modelLabelsSubject = new BehaviorSubject<CameraAIModelDTOLabel[]>([])
+  modelLabelsSubject = new BehaviorSubject<CameraAIModelDTOLabel[]>([]);
 
   get title() {
     if (this.state == FormState.add) {
@@ -62,54 +72,56 @@ export class AIModelOperateComponent implements OnInit, AfterViewInit, OnDestroy
     } else if (this.state == FormState.edit) {
       return '编辑' + this._cameraAIModel?.ModelName;
     }
-    return ''
+    return '';
   }
   // 切换选中Icon
   get selectedIcon() {
-    if (this.selectedIconValue)
-      return this.imgBase + this.selectedIconValue
+    if (this.selectedIconValue) return this.imgBase + this.selectedIconValue;
     return '';
   }
   get selectedIconValue() {
-    return this.iconsMap.get(this.selectedIconKey)
+    return this.iconsMap.get(this.selectedIconKey);
   }
 
   @Input()
   state: FormState = FormState.none;
 
-
   @Input()
   aiModelId: string = '';
 
   @Output()
-  closeEvent = new EventEmitter<boolean>()
+  closeEvent = new EventEmitter<boolean>();
 
   @ViewChild('fileBtn') fileBtn?: ElementRef<HTMLInputElement>;
 
   @ViewChild('mask') mask!: ElementRef<HTMLDivElement>;
 
-  constructor(private _business: AIModelOperateBusiness, private _toastrService: ToastrService, @Inject(DOCUMENT) private document: any) {
-  }
-
+  constructor(
+    private _business: AIModelOperateBusiness,
+    private _toastrService: ToastrService,
+    @Inject(DOCUMENT) private document: any
+  ) {}
 
   async ngOnInit() {
     if (this.state == FormState.edit) {
-      this._cameraAIModel = await this._business.getAIModel(this.aiModelId)
-      console.log('编辑', this._cameraAIModel)
+      this._cameraAIModel = await this._business.getAIModel(this.aiModelId);
+      console.log('编辑', this._cameraAIModel);
       this._aiModelDTo = this._cameraAIModel.ModelDTO;
-      this.modelLabelsSubject.next(this._aiModelDTo ? this._aiModelDTo.Labels : [])
+      this.modelLabelsSubject.next(
+        this._aiModelDTo ? this._aiModelDTo.Labels : []
+      );
     }
-    this.modelLabelsSubject.subscribe(labels => {
-      console.log('回调', labels)
+    this.modelLabelsSubject.subscribe((labels) => {
+      // console.log('回调', labels);
       // this._aiModelLabels = data;
-    })
-    this._updateForm()
+    });
+    this._updateForm();
   }
 
   ngAfterViewInit() {
     this.maskSub = fromEvent(this.mask.nativeElement, 'click').subscribe(() => {
       this.showList = false;
-    })
+    });
   }
   ngOnDestroy(): void {
     this.maskSub.unsubscribe();
@@ -117,138 +129,135 @@ export class AIModelOperateComponent implements OnInit, AfterViewInit, OnDestroy
   // 选择ModelJson文件
   triggerSelect() {
     if (this.fileBtn) {
-      this.fileBtn.nativeElement.click()
+      this.fileBtn.nativeElement.click();
     }
   }
   // 选中文件回调
   async selectFile(input: HTMLInputElement) {
     let filePath = input.value;
-    let fileContent = ''
+    let fileContent = '';
+
     if (input.files && input.files.length) {
-      fileContent = await this._readFileContent(input.files[0]) as string;
-      fileContent = fileContent.replace("data:application/json;base64,", '')
+      fileContent = (await this._readFileContent(input.files[0])) as string;
+      fileContent = fileContent.replace('data:application/json;base64,', '');
     } else {
-      this._aiModelDTo = void 0;
-      this.modelLabelsSubject.next([])
+      if (this.state == FormState.add) {
+        this._aiModelDTo = void 0;
+        this._parsedAIModel = void 0;
+        this.modelLabelsSubject.next([]);
+      } else {
+        this._aiModelDTo = this._cameraAIModel?.ModelDTO;
+        this.modelLabelsSubject.next(
+          this._aiModelDTo ? this._aiModelDTo.Labels : []
+        );
+      }
     }
     this.myForm.patchValue({
       FilePath: filePath,
-      ModelJson: fileContent
-    })
+      ModelJson: fileContent,
+    });
   }
   // 应用模型
   async parseFile() {
     let jsonData = this.myForm.value.ModelJson;
     if (jsonData) {
-      let res = await this._business.parseAIModel(jsonData) as CameraAIModel;
-      this._parsedCameraAIModel = res;
+      let res = (await this._business.parseAIModel(jsonData)) as CameraAIModel;
+      this._parsedAIModel = res;
       this._aiModelDTo = res.ModelDTO;
-      this.modelLabelsSubject.next(res.ModelDTO ? res.ModelDTO.Labels : [])
-      console.log('parsed', res)
+      this.modelLabelsSubject.next(res.ModelDTO ? res.ModelDTO.Labels : []);
+      console.log('parsed', res);
 
       this.myForm.patchValue({
         Version: res.Version,
-        TransformType: res.TransformType
-      })
+        TransformType: res.TransformType,
+      });
+    } else {
+      this._toastrService.warning('请选择模型文件');
     }
-
   }
   toggleList(e: Event) {
     this.showList = !this.showList;
     e.stopPropagation();
   }
   clickIcon(icon: [string, string], e: Event) {
-
     this.selectedIconKey = icon[0];
     this.myForm.patchValue({
-      Label: this.selectedIconKey.toString()
-    })
+      Label: this.selectedIconKey.toString(),
+    });
 
-    e.stopPropagation()
+    e.stopPropagation();
   }
 
   async onSubmit() {
     if (this._checkForm()) {
       if (this.state == FormState.add) {
-
         let model: CameraAIModel;
 
-        if (this._parsedCameraAIModel) {
+        if (this._parsedAIModel) {
           //如果已经解析过模型，则使用解析后的对象提交
-          model = this._parsedCameraAIModel
-          this._parsedCameraAIModel.ModelName = this.myForm.value.ModelName ?? '';
-          this._parsedCameraAIModel.Label = + (this.myForm.value.Label ?? '');
+          model = this._parsedAIModel;
+          this._parsedAIModel.ModelName = this.myForm.value.ModelName ?? '';
+          this._parsedAIModel.ModelType = this.myForm.value.ModelType ?? '';
+          this._parsedAIModel.Label = +(this.myForm.value.Label ?? '');
         } else {
           model = new CameraAIModel();
           model.Id = '';
           model.ModelName = this.myForm.value.ModelName ?? '';
           model.ModelType = this.myForm.value.ModelType ?? '';
-          model.ModelJSON = this.myForm.value.ModelJson ?? "";
-          model.Label = + (this.myForm.value.Label ?? '');
+          model.ModelJSON = this.myForm.value.ModelJson ?? '';
+          model.Label = +(this.myForm.value.Label ?? '');
           model.CreateTime = new Date().toISOString();
           model.UpdateTime = new Date().toISOString();
         }
 
-        let res = await this._business.createAIModel(model)
+        let res = await this._business.createAIModel(model);
         if (res) {
           this._toastrService.success('添加成功');
-          this.closeEvent.emit(true)
+          this.closeEvent.emit(true);
         }
-
       } else if (this.state == FormState.edit) {
         if (this._cameraAIModel) {
-          let model: CameraAIModel;
-          if (this._parsedCameraAIModel) {
-            model = this._parsedCameraAIModel
-            this._parsedCameraAIModel.ModelName = this.myForm.value.ModelName ?? '';
-            this._parsedCameraAIModel.Label = + (this.myForm.value.Label ?? '');
-          } else {
-            model = this._cameraAIModel;
-            this._cameraAIModel.ModelName = this.myForm.value.ModelName ?? '';
-            this._cameraAIModel.ModelType = this.myForm.value.ModelType ?? '';
-            this._cameraAIModel.ModelJSON = this.myForm.value.ModelJson ?? ""
-            this._cameraAIModel.Label = + (this.myForm.value.Label ?? '');
-            this._cameraAIModel.UpdateTime = new Date().toISOString();
+          // 解析后的对象Id是'',要用 _cameraAIModel 提交
+          this._cameraAIModel.ModelName = this.myForm.value.ModelName ?? '';
+          this._cameraAIModel.ModelType = this.myForm.value.ModelType ?? '';
+          this._cameraAIModel.Label = +(this.myForm.value.Label ?? '');
+          this._cameraAIModel.UpdateTime = new Date().toISOString();
+          if (this._parsedAIModel) {
+            this._cameraAIModel.ModelJSON = '';
+            this._cameraAIModel.ModelDTO = this._parsedAIModel.ModelDTO;
           }
 
-
-          // this._cameraAIModel.ModelJSON = this._parsedCameraAIModel?.ModelJSON ?? "";
-          // this._cameraAIModel.ModelDTO = this._parsedCameraAIModel?.ModelDTO
-
-          let res = await this._business.setAIModel(model)
+          let res = await this._business.setAIModel(this._cameraAIModel);
           if (res) {
             this._toastrService.success('编辑成功');
-            this.closeEvent.emit(true)
+            this.closeEvent.emit(true);
           }
         }
       }
     }
   }
   onReset() {
-    this.closeEvent.emit(false)
+    this.closeEvent.emit(false);
   }
 
   modify() {
     if (this._cameraAIModel && this._cameraAIModel.ModelDTO) {
-      this._cameraAIModel.ModelDTO.Labels[0].LabelModelValue = '10'
+      this._cameraAIModel.ModelDTO.Labels[0].LabelModelValue = '10';
     }
   }
   private _readFileContent(file: File) {
     return new Promise((resolve, reject) => {
-      if (!file)
-        reject('No File!!!');
+      if (!file) reject('No File!!!');
 
       let reader = new FileReader();
       reader.addEventListener('load', function () {
-        resolve(reader.result)
-      })
-      reader.readAsDataURL(file)
-    })
+        resolve(reader.result);
+      });
+      reader.readAsDataURL(file);
+    });
   }
   private _updateForm() {
     if (this.state == FormState.add) {
-
-
     } else if (this.state == FormState.edit) {
       if (this._cameraAIModel) {
         this.myForm.patchValue({
@@ -256,8 +265,8 @@ export class AIModelOperateComponent implements OnInit, AfterViewInit, OnDestroy
           ModelType: this._cameraAIModel.ModelType,
           Version: this._cameraAIModel.Version,
           TransformType: this._cameraAIModel.TransformType,
-          Label: this._cameraAIModel.Label.toString()
-        })
+          Label: this._cameraAIModel.Label.toString(),
+        });
         this.selectedIconKey = this._cameraAIModel.Label.toString();
       }
     }
@@ -269,7 +278,7 @@ export class AIModelOperateComponent implements OnInit, AfterViewInit, OnDestroy
     }
     if (this.state == FormState.add) {
       if (this.myForm.get('FilePath')?.invalid) {
-        this._toastrService.warning('请选择模型');
+        this._toastrService.warning('请选择模型文件');
         return;
       }
     }
@@ -277,7 +286,6 @@ export class AIModelOperateComponent implements OnInit, AfterViewInit, OnDestroy
       this._toastrService.warning('请选择模型图标');
       return;
     }
-
 
     return true;
   }
--- src/app/common/components/ai-model-tree/ai-model-tree.component.ts
+++ src/app/common/components/ai-model-tree/ai-model-tree.component.ts
@@ -1,10 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { time } from 'console';
 import { TreeConverter } from 'src/app/converter/tree.converter';
-import { CameraAIModel, CameraAIModelDTOLabel, EnumValue } from 'src/app/network/model/camera-ai.model';
+import {
+  CameraAIModel,
+  CameraAIModelDTOLabel,
+  EnumValue,
+} from 'src/app/network/model/camera-ai.model';
 import { AIModelTreeBusiness } from './ai-model-tree.business';
 import { AIModelTreeConverter } from '../../../converter/ai-model-tree.converter';
-import { MatTreeFlatDataSource, MatTreeFlattener, MatTreeNestedDataSource } from '@angular/material/tree';
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener,
+  MatTreeNestedDataSource,
+} from '@angular/material/tree';
 import { AIModelNestNode } from 'src/app/view-model/ai-model-nest-node.model';
 import { FlatTreeControl, NestedTreeControl } from '@angular/cdk/tree';
 import { AIModelFlatNode } from 'src/app/view-model/ai-model-flat-node.model';
@@ -17,22 +25,14 @@ type rawDataType = CameraAIModelDTOLabel | EnumValue;
   selector: 'howell-ai-model-tree',
   templateUrl: './ai-model-tree.component.html',
   styleUrls: ['./ai-model-tree.component.less'],
-  providers: [
-    AIModelTreeBusiness,
-    AIModelTreeConverter
-  ]
+  providers: [AIModelTreeBusiness, AIModelTreeConverter],
 })
 export class AIModelTreeComponent implements OnInit {
-
   private _flatNodeMap = new Map<string, AIModelFlatNode<rawDataType>>();
-  private _transformer = (nestNode: AIModelNestNode<rawDataType>, level: number) => {
-    const existingNode = this._flatNodeMap.get(nestNode.id);
-
-    if (existingNode) {
-      existingNode.name = nestNode.name;
-      existingNode.expandable = nestNode.hasChild;
-      return existingNode;
-    }
+  private _transformer = (
+    nestNode: AIModelNestNode<rawDataType>,
+    level: number
+  ) => {
     const flatNode = new AIModelFlatNode<rawDataType>();
     flatNode.id = nestNode.id;
     flatNode.name = nestNode.name;
@@ -53,49 +53,57 @@ export class AIModelTreeComponent implements OnInit {
     return flatNode;
   };
   private _getLevel = (node: AIModelFlatNode<rawDataType>) => node.level;
-  private _isExpandable = (node: AIModelFlatNode<rawDataType>) => node.expandable;
-  private _getChildren = (node: AIModelNestNode<rawDataType>) => node.childrenChange;
-  private _hasChild = (index: number, node: AIModelFlatNode<rawDataType>) => node.expandable;
+  private _isExpandable = (node: AIModelFlatNode<rawDataType>) =>
+    node.expandable;
+  private _getChildren = (node: AIModelNestNode<rawDataType>) =>
+    node.childrenChange;
+  private _hasChild = (index: number, node: AIModelFlatNode<rawDataType>) =>
+    node.expandable;
   private dataChange = new BehaviorSubject<AIModelNestNode[]>([]);
   private _nestedNodeMap = new Map<string, AIModelNestNode>();
   private _currentNode: AIModelFlatNode<rawDataType> | null = null;
 
-
-  private _treeFlattener: MatTreeFlattener<AIModelNestNode<rawDataType>, AIModelFlatNode<rawDataType>>;
+  private _treeFlattener: MatTreeFlattener<
+    AIModelNestNode<rawDataType>,
+    AIModelFlatNode<rawDataType>
+  >;
   private _aiModelLabels: CameraAIModelDTOLabel[] = [];
 
-
-
   treeControl: FlatTreeControl<AIModelFlatNode<rawDataType>>;
-  dataSource: MatTreeFlatDataSource<AIModelNestNode<rawDataType>, AIModelFlatNode<rawDataType>>;
+  dataSource: MatTreeFlatDataSource<
+    AIModelNestNode<rawDataType>,
+    AIModelFlatNode<rawDataType>
+  >;
   trackBy = (index: number, node: AIModelFlatNode<rawDataType>) => node;
-  selection!: SelectionModel<AIModelFlatNode<rawDataType>>;// 保存选中节点
+  selection!: SelectionModel<AIModelFlatNode<rawDataType>>; // 保存选中节点
   // 高亮显示选中节点
   highLight = (node: AIModelFlatNode<rawDataType>) => {
     return this.selection.isSelected(node);
   };
 
   @Input()
-  modelLabelsSubject = new BehaviorSubject<CameraAIModelDTOLabel[]>([])
+  modelLabelsSubject = new BehaviorSubject<CameraAIModelDTOLabel[]>([]);
 
-
-  constructor(private _business: AIModelTreeBusiness, private _converter: AIModelTreeConverter) {
+  constructor(
+    private _business: AIModelTreeBusiness,
+    private _converter: AIModelTreeConverter
+  ) {
     this._treeFlattener = new MatTreeFlattener(
       this._transformer,
       this._getLevel,
       this._isExpandable,
       this._getChildren
-    )
+    );
 
     this.treeControl = new FlatTreeControl<AIModelFlatNode<rawDataType>>(
       this._getLevel,
       this._isExpandable
     );
 
-    this.dataSource = new MatTreeFlatDataSource<AIModelNestNode<rawDataType>, AIModelFlatNode<rawDataType>>(
-      this.treeControl,
-      this._treeFlattener
-    );
+    this.dataSource = new MatTreeFlatDataSource<
+      AIModelNestNode<rawDataType>,
+      AIModelFlatNode<rawDataType>
+    >(this.treeControl, this._treeFlattener);
 
     this.dataChange.subscribe((data) => {
       this.dataSource.data = data;
@@ -104,19 +112,18 @@ export class AIModelTreeComponent implements OnInit {
   }
 
   async ngOnInit() {
-    this.modelLabelsSubject.subscribe(data => {
+    this.modelLabelsSubject.subscribe((data) => {
       this._aiModelLabels = data;
       let res = this._converter.recurseToNestTreeNode(data);
       this.dataChange.next(res);
-    })
-
+    });
   }
   selectNode(node: AIModelFlatNode<CameraAIModelDTOLabel>) {
     if (this.selection.isSelected(node)) return;
     this.selection.toggle(node);
   }
   click() {
-    console.log(this._flatNodeMap)
+    console.log(this._flatNodeMap);
   }
   touchSpinChange(data: string, node: AIModelFlatNode<rawDataType>) {
     // console.log('touchSpinChange', data)
@@ -127,8 +134,6 @@ export class AIModelTreeComponent implements OnInit {
       node.rawData.ModelValue = +data;
     }
     // 虽然修改的是对象，显式提示数据更新
-    this.modelLabelsSubject.next(this._aiModelLabels)
-
+    this.modelLabelsSubject.next(this._aiModelLabels);
   }
-
 }
