21443125d8a89f84ffee0974a344814ec7ed4818
panminxiang <1061886912@qq.com>
2022-10-10 13:41:39 +0800

    pmx

--- src/app/common/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/common/components/confirm-dialog/confirm-dialog.component.ts
@@ -2,28 +2,26 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { DialogEnum } from 'src/app/enum/dialog.enum';
 import { ConfirmDialogModel } from 'src/app/view-model/confirm-dialog.model';
 
-
 @Component({
   selector: 'howell-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
-  styleUrls: ['./confirm-dialog.component.less']
+  styleUrls: ['./confirm-dialog.component.less'],
 })
 export class ConfirmDialogComponent implements OnInit {
-
-  @Input() dialogModel: ConfirmDialogModel = new ConfirmDialogModel('确认删除', '删除该项');
+  @Input() dialogModel: ConfirmDialogModel = new ConfirmDialogModel(
+    '确认删除',
+    '删除该项'
+  );
 
   @Output() dialogMsgEvent = new EventEmitter<DialogEnum>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   confirmClick() {
-    this.dialogMsgEvent.emit(DialogEnum.confirm)
+    this.dialogMsgEvent.emit(DialogEnum.confirm);
   }
   cancelClick() {
-    this.dialogMsgEvent.emit(DialogEnum.cancel)
-
+    this.dialogMsgEvent.emit(DialogEnum.cancel);
   }
-
 }
