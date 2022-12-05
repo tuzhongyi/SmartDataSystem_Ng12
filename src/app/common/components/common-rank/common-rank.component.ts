f9e485039a479845d557f13e35f4653ac60edf5e
panminxiang <1061886912@qq.com>
2022-12-05 13:36:44 +0800

    排行榜

---
+++ src/app/common/components/common-rank/common-rank.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'common-rank',
+  templateUrl: './common-rank.component.html',
+  styleUrls: ['./common-rank.component.less']
+})
+export class CommonRankComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
