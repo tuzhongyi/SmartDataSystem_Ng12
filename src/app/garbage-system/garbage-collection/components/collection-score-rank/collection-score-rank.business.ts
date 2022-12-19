475f7969dc74ce1297a976f94776c0f7c3a97bee
panminxiang <1061886912@qq.com>
2022-12-19 16:28:55 +0800

    优化

--- src/app/garbage-system/garbage-collection/components/collection-score-rank/collection-score-rank.business.ts
+++ src/app/garbage-system/garbage-collection/components/collection-score-rank/collection-score-rank.business.ts
@@ -41,16 +41,19 @@ export class CollectionScoreRankBusiness {
     let { Data } = await this._listScores(searchInfo);
     // console.log(Data);
 
-    // for (let i = 0; i < 10; i++) {
-    //   let scoreTop = new ScoreTop();
-    //   scoreTop.Id = uuid.v4();
-    //   let unicode1 = Math.floor(Math.random() * (0x9fa5 - 0x4e00) + 0x4e00);
-    //   let unicode2 = Math.floor(Math.random() * (0x9fa5 - 0x4e00) + 0x4e00);
-    //   let unicode3 = Math.floor(Math.random() * (0x9fa5 - 0x4e00) + 0x4e00);
-    //   scoreTop.Name = String.fromCodePoint(unicode1, unicode2, unicode3);
-    //   scoreTop.Number = (Math.random() * 100) >> 0;
-    //   Data.push(scoreTop);
-    // }
+    for (let i = 0; i < 10; i++) {
+      let scoreTop = new ScoreTop();
+      scoreTop.Id = uuid.v4();
+      let unicode1 = Math.floor(Math.random() * (0x9fa5 - 0x4e00) + 0x4e00);
+      let unicode2 = Math.floor(Math.random() * (0x9fa5 - 0x4e00) + 0x4e00);
+      let unicode3 = Math.floor(Math.random() * (0x9fa5 - 0x4e00) + 0x4e00);
+      scoreTop.Name = String.fromCodePoint(unicode1, unicode2, unicode3);
+      scoreTop.Number = (Math.random() * 100) >> 0;
+      Data.push(scoreTop);
+    }
+    Data.sort((a, b) => {
+      return (a.Number - b.Number) * -1;
+    });
     let res = this._converter.Convert(Data);
 
     return res;
